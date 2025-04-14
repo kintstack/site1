7c571855048b3db2affbc08b68652e2a0ef4b3c4
AnonymousError14 <kintjohn12@gmail.com>
2025-04-14 10:09:46 +0800

    Update database name in .env and organize user routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,19 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('/users', 'UserController@add'); // 👈 not users1
+});
+
 $router->get('/test', function () {
     return response()->json(['message' => 'API is working!']);
 });
 
 $router->group(['prefix' => 'api'], function () use ($router) {
-    $router->get('/users', 'UserController@index');        // Get all users
-    $router->post('/users', 'UserController@add');         // Create a new user
-    $router->get('/users/{id}', 'UserController@show');    // Get user by ID
-    $router->put('/users/{id}', 'UserController@update');  // Update user
-    $router->patch('/users/{id}', 'UserController@update');// Update user (partial)
-    $router->delete('/users/{id}', 'UserController@delete');// Delete user
+    $router->post('/users', 'UserController@add'); // POST request for adding users
+    $router->get('/users', 'UserController@index'); // Get all users
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Update user (partial)
+    $router->delete('/users/{id}', 'UserController@delete'); // Delete user
 });
